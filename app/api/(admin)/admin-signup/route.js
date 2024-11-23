e523ea641a5c196626387063aa03a70df18cdb15
Brian <arce.jhonbrian.dev@gmail.com>
2024-11-23 13:50:45 +0800

    added admin signup

--- app/api/(admin)/admin-signup/route.js
+++ app/api/(admin)/admin-signup/route.js
@@ -1,20 +1,20 @@
 import { connectMongoDB } from "@/lib/mongodb";
-import Student from "@/models/student";
 import { NextResponse } from "next/server";
 import bcrypt from 'bcryptjs'
+import Admin from "@/models/admin";
 
 export async function POST(req) {
     try {
-        const {studentNo, email, password } = await req.json()
+        const {email, password } = await req.json()
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
         await connectMongoDB()
-        await Student.create({studentNo, email, password: hashedPassword})
+        await Admin.create({ email, password: hashedPassword})
 
-        return NextResponse.json({message : "Student created successfully"}, {status : 201})
+        return NextResponse.json({message : "Admin created successfully"}, {status : 201})
     } catch (error) {
         console.log("Error: ", error)
-        return NextResponse.json({message: "An error occured while creating student"}, {status : 500})
+        return NextResponse.json({message: "An error occured while creating admint"}, {status : 500})
     }
 }
